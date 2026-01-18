b495c1168e3e1599c7ab7601841e73ccd76a32e3
dailyhotcoolmeme <dailyhotcoolmeme@gmail.com>
2026-01-18 22:19:59 +0900

    Fix import paths

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,7 @@
 import { Switch, Route, useLocation } from "wouter";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/queryClient";
-//import { Toaster } from "@/components/ui/toaster";
-import { Layout } from "@/components/Layout";
+import { Layout } from "./components/Layout";
 import { DisplaySettingsProvider } from "@/components/DisplaySettingsProvider";
 import { TopBar } from "@/components/TopBar";
 import { BottomNav } from "@/components/BottomNav";
