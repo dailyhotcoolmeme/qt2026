842619377b74a36095f8b2a082ce35b3afd6156a
dailyhotcoolmeme <dailyhotcoolmeme@gmail.com>
2026-01-18 23:30:57 +0900

    main

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,11 @@
+import React from "react"; // 1. 이 줄을 맨 위에 추가했습니다.
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+// React.StrictMode로 감싸주면 오류를 잡기에 더 좋습니다.
+createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
