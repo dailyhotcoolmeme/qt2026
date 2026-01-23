0d4fc5011167ff793f168c736a028b15be7d10ff
dailyhotcoolmeme <dailyhotcoolmeme@gmail.com>
2026-01-23 09:00:51 +0900

    Update DailyWordPage.tsx

--- client/src/pages/DailyWordPage.tsx
+++ client/src/pages/DailyWordPage.tsx
@@ -145,7 +145,7 @@ export default function DailyWordsPage() {
           input: { text: textToSpeak },
           voice: { 
             languageCode: "ko-KR", 
-            name: voiceType === 'M' ? "ko-KR-Neural2-C" : "ko-KR-Neural2-B" 
+            name: voiceType === 'F' ? "ko-KR-Neural2-C" : "ko-KR-Neural2-B" 
           },
           audioConfig: { audioEncoding: "MP3",speakingRate: 0.9 },
         }),
