76626eef7591ff574b33a6d6b8cd6e1584f37613
dogstory2026 <dogstory2026@gmail.com>
2026-03-19 23:54:44 +0900

    임시: App.tsx 카카오 초기화 제거 (APK 테스트용)

--- App.tsx
+++ App.tsx
@@ -3,8 +3,6 @@ import { QueryClientProvider } from '@tanstack/react-query'
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 import { StyleSheet } from 'react-native'
-import { initializeKakaoSDK } from '@react-native-kakao/core'
-
 import { queryClient } from 'src/lib/queryClient'
 import { linking } from 'src/navigation/linking'
 import { RootNavigator } from 'src/navigation/RootNavigator'
@@ -12,11 +10,6 @@ import { configureNotificationHandler } from 'src/lib/notifications'
 import { usePushNotifications } from 'src/hooks/notifications/usePushNotifications'
 
 configureNotificationHandler()
-
-const KAKAO_NATIVE_APP_KEY = process.env.KAKAO_NATIVE_APP_KEY ?? ''
-if (KAKAO_NATIVE_APP_KEY) {
-  initializeKakaoSDK(KAKAO_NATIVE_APP_KEY)
-}
 
 function AppContent() {
   usePushNotifications()
